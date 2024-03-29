c277d348c4c0b9e7e1a249d6fe6f853f01c1ab9a
FRNInfoII <frn_info2@yahoo.com>
2024-03-29 15:32:41 -0300

    mul function implemented
    Not tested.

--- frn_complex.c
+++ frn_complex.c
@@ -17,3 +17,12 @@ struct Complex sub(struct Complex z1,
     res.im = z1.im - z2.im;
     return res;
 }
+
+struct Complex mul(struct Complex z1,
+                   struct Complex z2)
+{
+    struct Complex res;
+    res.re = z1.re * z2.re - z1.im * z2.im;
+    res.im = z1.re * z2.im + z1.im * z2.re;
+    return res;
+}
--- frn_complex.h
+++ frn_complex.h
@@ -11,3 +11,6 @@ struct Complex add(struct Complex z1,
 
 struct Complex sub(struct Complex z1,
                    struct Complex z2);
+
+struct Complex mul(struct Complex z1,
+                   struct Complex z2);                   
