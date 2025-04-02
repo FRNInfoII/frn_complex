8437bd006b049ddfbf08113ea73608c29113843f
FRNInfoII <frn_info2@yahoo.com>
2025-04-02 09:56:34 -0300

    div added

--- frn_complex.c
+++ frn_complex.c
@@ -26,3 +26,36 @@ struct Complex mul(struct Complex z1,
     res.im = z1.re * z2.im + z1.im * z2.re;
     return res;
 }
+
+struct Complex div(struct Complex z1,
+                   struct Complex z2)
+{
+    struct Complex res = mul( z1 , conj(z2) );
+    res = real_div( res, mul( z2 , conj(z2) ).re );
+    return res;
+}
+
+struct Complex real_mul(struct Complex z1,
+         double scalar)
+{
+    struct Complex res = z1;
+    res.im *= scalar;
+    res.re *= scalar;
+    return res;
+}
+        
+struct Complex real_div(struct Complex z1,
+         double scalar)
+{
+    struct Complex res = z1;
+    res.im /= scalar;
+    res.re /= scalar;
+    return res;
+}
+
+struct Complex conj(struct Complex z1)
+{
+    struct Complex res = z1;
+    res.im *= -1.0;
+    return res;
+}
--- frn_complex.h
+++ frn_complex.h
@@ -13,4 +13,16 @@ struct Complex sub(struct Complex z1,
                    struct Complex z2);
 
 struct Complex mul(struct Complex z1,
-                   struct Complex z2);                   
+                   struct Complex z2);
+
+struct Complex div(struct Complex z1,
+                   struct Complex z2);
+
+struct Complex real_mul(struct Complex z1,
+                        double scalar);
+
+struct Complex real_div(struct Complex z1,
+                        double scalar);
+    
+struct Complex conj(struct Complex z1);
+      
